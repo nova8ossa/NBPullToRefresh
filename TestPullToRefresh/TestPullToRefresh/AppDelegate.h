e5674fd739287c06dcb4d7b4982022b32f51a24f
梵天 <fantian@xiaokakeji.com>
2015-07-29 15:44:38 +0800

    add demo project

---
+++ TestPullToRefresh/TestPullToRefresh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestPullToRefresh
+//
+//  Created by NOVA8OSSA on 15/7/29.
+//  Copyright (c) 2015年 NB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestPullToRefresh/TestPullToRefresh/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestPullToRefresh
+//
+//  Created by NOVA8OSSA on 15/7/29.
+//  Copyright (c) 2015年 NB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
